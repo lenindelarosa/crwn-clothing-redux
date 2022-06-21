16657f51490f2e379cf9e1c9077b91157f94551d
Lenin <leninmanueld@gmail.com>
2022-06-21 10:12:09 -0400

    adding registration to ServiceWorker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
 import App from './App';
 import { store, persistor } from './store/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import reportWebVitals from './reportWebVitals';
 
@@ -30,3 +31,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
+serviceWorkerRegistration.register();
