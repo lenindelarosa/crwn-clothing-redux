b8b4a8e7604f1324edee3090ac24dcc669cf8350
Lenin <leninmanueld@gmail.com>
2022-06-21 11:55:22 -0400

    home page now uses local images

--- src/components/categories-menu/categories-menu.component.jsx
+++ src/components/categories-menu/categories-menu.component.jsx
@@ -1,31 +1,36 @@
 import { CategoriesContainer } from './categories-menu.styles'
 import DirectoryItem from '../directory-item/directory-item.component';
+import Hats from '../../assets/images/hats.png';
+import Jackets from '../../assets/images/jackets.png';
+import Sneakers from '../../assets/images/sneakers.png';
+import Womens from '../../assets/images/womens.png';
+import Mens from '../../assets/images/men.png';
 
 const categories = [
   {
     "id": 1,
     "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    "imageUrl": Hats
   },
   {
     "id": 2,
     "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    "imageUrl": Jackets
   },
   {
     "id": 3,
     "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    "imageUrl": Sneakers
   },
   {
     "id": 4,
     "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    "imageUrl": Womens
   },
   {
     "id": 5,
     "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    "imageUrl": Mens
   }
 ];
 const CategoriesMenu = () => {
